6ffa4e926485547b22368745e268a17290c4d3ce
biswa-ganguly <biswaganguly16@gmail.com>
2025-06-20 21:48:55 +0530

    fix

--- backend/src/routes/hospitalFetchRoute.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import fetchHospitals from '../controller/fetchController.js';
-
-const router = express.Router();
-
-router.get('/fetch-hospitals', fetchHospitals);
-
-export default router;
--- backend/src/routes/policeStationRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createPoliceStation, policeLogin } from "../controller/policeStationController.js";
-
-const router = express.Router();
-
-router.post('/register', createPoliceStation);
-router.post('/login', policeLogin);
-
-export default router;
